212cddccaaec0f683b4e2630382f487f52185dbd
SlothyDev <108906796+SlothyDev@users.noreply.github.com>
2026-07-03 20:45:18 -0400

    Mac support

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,12 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID } from '@env';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
   storageBucket: "iron-app-87b7b.firebasestorage.app",
   messagingSenderId: "43392954526",
   appId: "1:43392954526:web:b42368b0a8d62f22c4cb8f"
